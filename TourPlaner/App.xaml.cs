b4216551516a1d749cd5a4ab7f5a05bd4ab6d303
HelmutSoellinger <113792719+HelmutSoellinger@users.noreply.github.com>
2024-03-13 12:11:14 +0100

    TourlogEditbutton + EditButtonViewmodel (Ansatz für TourEditButton)MVVM + command bindings(errors)

--- TourPlaner/App.xaml.cs
+++ TourPlaner/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using TourPlaner.ViewModels;
+using TourPlaner.Views;
 
 namespace TourPlaner
 {
@@ -9,6 +11,24 @@ namespace TourPlaner
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var logButtonsViewModel = new EditButtonViewModel();
+            var tourButtonsViewModel = new EditButtonViewModel();
+            var tourViewModel = new TourViewModel(logButtonsViewModel,tourButtonsViewModel);
+            var mainWindow = new MainWindow 
+            {
+                DataContext = tourViewModel, 
+                LogButtons = { 
+                    DataContext = logButtonsViewModel 
+                },
+                TourButtons =
+                {
+                    DataContext = tourButtonsViewModel 
+                }
+            };
+            mainWindow.Show();
+        }
     }
 
 }
--- TourPlaner/ViewModels/RelayCommand.cs
+++ TourPlaner/ViewModels/RelayCommand.cs
@@ -1,6 +1,6 @@
 ﻿using System.Windows.Input;
 
-namespace TourPlaner
+namespace TourPlaner.ViewModels
 {
     public class RelayCommand : ICommand
     {
